fe63a0d2dedf1da89dda5ee3c48ed061ba815b68
suica <578970498@qq.com>
2021-01-19 00:42:57 +0800

    solve: 10 again

--- questions/10-medium-tuple-to-union/template.ts
+++ questions/10-medium-tuple-to-union/template.ts
@@ -1,1 +1,2 @@
-type TupleToUnion<T extends any[]> = T extends [...(infer A), infer C] ? (unknown extends C ? never : C | TupleToUnion<A>) : T;
+type _TupleToUnion<T extends any[]> = T extends [...(infer A), infer C] ? (unknown extends C ? never : C | _TupleToUnion<A>) : T;
+type TupleToUnion<T extends any[]> = T extends [infer A, ...(infer C)] ? (unknown extends A ? never : A | TupleToUnion<C>) : never;
