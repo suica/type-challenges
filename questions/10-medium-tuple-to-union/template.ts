a6eacb9430f70d46f69e8e5dc6e0f5d96972ff96
suica <578970498@qq.com>
2021-01-19 01:03:52 +0800

    fix: 10

--- questions/10-medium-tuple-to-union/template.ts
+++ questions/10-medium-tuple-to-union/template.ts
@@ -1,2 +1,2 @@
-type _TupleToUnion<T extends any[]> = T extends [...(infer A), infer C] ? (unknown extends C ? never : C | _TupleToUnion<A>) : T;
-type TupleToUnion<T extends any[]> = T extends [infer A, ...(infer C)] ? (unknown extends A ? never : A | TupleToUnion<C>) : never;
+type _TupleToUnion<T extends readonly any[]> = T extends readonly [...(infer A), infer C] ? (unknown extends C ? never : C | _TupleToUnion<A>) : T;
+type TupleToUnion<T extends readonly any[]> = T extends readonly [infer A, ...(infer C)] ? (unknown extends A ? never : A | TupleToUnion<C>) : never;
