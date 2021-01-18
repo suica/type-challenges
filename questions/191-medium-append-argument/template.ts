5d8eaa495b190716d9d1b32baf381d5273f4325a
suica <578970498@qq.com>
2021-01-19 01:32:12 +0800

    solve: 191

--- questions/191-medium-append-argument/template.ts
+++ questions/191-medium-append-argument/template.ts
@@ -1,1 +1,2 @@
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn, A> = Fn extends (...args: infer R) => infer T ? (...args: [...R, A]) => T : never
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
