05ee66dd0e7a4e072c0acf54108d006f1269aa0d
suica <578970498@qq.com>
2021-01-19 20:03:02 +0800

    solve: 599

--- questions/599-medium-merge/template.ts
+++ questions/599-medium-merge/template.ts
@@ -1,1 +1,11 @@
-type Merge<F, S> = any;
+type Merge<F, S> = { [x in keyof (F & S)]: x extends (keyof (F | S)) ? S[x] : (x extends keyof F ? F[x] : never) }
+
+type Foo = {
+    a: number;
+    b: string;
+};
+type Bar = {
+    b: number;
+};
+
+type a = Merge<Foo, Bar>
