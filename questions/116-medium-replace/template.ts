597a268275b6d55e3a1934fee30953274baa765b
suica <578970498@qq.com>
2021-01-19 20:26:31 +0800

    solve: 116

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -1,1 +1,1 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> = From extends '' ? S : (S extends `${infer Prefix}${From}${infer Suffix}` ? `${Prefix}${To}${Suffix}` : S)
