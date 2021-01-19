7629f5b1441b7bc7cc1b5dc3e5bc703a5eae291a
suica <578970498@qq.com>
2021-01-20 00:25:04 +0800

    solve: 55

--- questions/55-hard-union-to-intersection/template.ts
+++ questions/55-hard-union-to-intersection/template.ts
@@ -1,1 +1,7 @@
-type UnionToIntersection<U> = any
+// A wrong solution, but don't know why
+
+// import {Permutation} from "../296-medium-permutation/template";
+// type TupleToIntersection<T> = T extends [infer First, ...infer Rest] ? First & TupleToIntersection<Rest> : unknown
+// export type UnionToIntersection<U> = Permutation<U> extends infer A ?[A, TupleToIntersection<A>] : never;
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
