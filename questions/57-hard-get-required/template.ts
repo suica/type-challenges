da07476e170123fae4cd6807a1bccb86f3734a41
suica <578970498@qq.com>
2021-01-19 22:51:47 +0800

    solve: 57

--- questions/57-hard-get-required/template.ts
+++ questions/57-hard-get-required/template.ts
@@ -1,1 +1,6 @@
-type GetRequired<T> = any
+const deleteSymbol = Symbol();
+type MarkAsDeleteSymbol<T> = { [x in keyof T]-?: Partial<Pick<T, x>> extends Pick<T, x> ? typeof deleteSymbol : T[x] }
+type GetDeletedKeys<T> = keyof T extends infer x ? (x extends keyof MarkAsDeleteSymbol<T> ? (MarkAsDeleteSymbol<T>[x] extends typeof deleteSymbol ? never : x) : 1) : 2
+type GetRequired<T> = GetDeletedKeys<T> extends keyof T ? Pick<T, GetDeletedKeys<T>> : never
+
+// 这里用到了一个非常重要的模式：marked object to keys
