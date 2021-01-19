834fa0ee6287e96009a80f75fd3a806d55d9d01b
suica <578970498@qq.com>
2021-01-19 23:59:38 +0800

    solve: 296

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,2 @@
-type Permutation<T> = any
+import {Equal} from "@type-challenges/utils";
+export type Permutation<U, P = U> = Equal<U, never> extends true ? [] : (P extends infer A ? ([...Permutation<Exclude<U, A>> extends never ? [] : Permutation<Exclude<U, A>>, A]) : never)
--- questions/296-medium-permutation/test-cases.ts
+++ questions/296-medium-permutation/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Permutation<U, P = U> = Equal<U, never> extends true ? [] : (P extends infer A ? ([...Permutation<Exclude<U, A>> extends never ? [] : Permutation<Exclude<U, A>>, A]) : never)
+
 type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
