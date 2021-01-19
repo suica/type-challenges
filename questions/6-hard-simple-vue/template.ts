c65ea4fb236c737a25e6379b33b24f69869b8637
suica <578970498@qq.com>
2021-01-19 21:44:46 +0800

    solve: 6

--- questions/6-hard-simple-vue/template.ts
+++ questions/6-hard-simple-vue/template.ts
@@ -1,2 +1,9 @@
+type Desc<D, M, C> = {
+    data?: () => D;
+    methods?: M & ThisType<D & M & { [x in keyof C]: C[x] extends (...args: any[]) => any ? ReturnType<C[x]> : never }>;
+    computed?: C & ThisType<D & M>;
+}
 
-declare function SimpleVue(options: any): any
+declare function SimpleVue<D, M, C>(options: Desc<D, M, C>): D & M & C
+
+declare function alert(a: any): void;
