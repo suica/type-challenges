052a8aa248e52798f71c7363b6259ac59537f255
suica <578970498@qq.com>
2021-01-19 19:50:15 +0800

    solve: 645 with comment

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,12 +1,21 @@
-type Diff<O, O1> = any
+type Diff<O, O1> = Omit<O & O1, keyof (O | O1)>
+
+// 类型是具有某一种标签的所有物件的(可为无穷)集合
+// 两个类型的并可以看成是两个无穷集的并
+// 两个object的字段并可以写成
+type a = keyof (Foo & Bar) // 'name'|'age'|'gender'
+// 两个object的字段交可以写成
+type b = keyof (Foo | Bar) // 'name'|'age'
+// 因此只需要从并中剔除交就得到了对称差
+type c = Exclude<a, b>
 
 type Foo = {
-  name: string
-  age: string
+    name: string
+    age: string
 }
 type Bar = {
-  name: string
-  age: string
-  gender: number
+    name: string
+    age: string
+    gender: number
 }
 type test0 = Diff<Foo, Bar>   // { gender: number }
