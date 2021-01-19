cad2903463e5c7aba78a0263e2fe5a722242f444
suica <578970498@qq.com>
2021-01-20 01:08:56 +0800

    solve: 298

--- questions/298-medium-length-of-string/template.ts
+++ questions/298-medium-length-of-string/template.ts
@@ -1,1 +1,3 @@
-type LengthOfString<S extends string> = any
+// @ts-expect-error
+type LengthOfString<S extends string> = LengthHelper<S>['length']
+type LengthHelper<T extends string> = T extends `${infer A}${infer B}`?[A,...LengthHelper<B>]:[];
