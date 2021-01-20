42d7f4f08086584068062ddd99d008263b586794
suica <578970498@qq.com>
2021-01-20 22:13:09 +0800

    solve: 399

--- questions/399-hard-tuple-filter/template.ts
+++ questions/399-hard-tuple-filter/template.ts
@@ -1,1 +1,7 @@
-type FilterOut<T extends any[], F> = any
+import {Equal} from "@type-challenges/utils";
+type ShouldBeFiltered<From, F> = Equal<From, F> extends true ?
+    (Equal<F, never> extends true ? true : false) : (From extends F ? true : false)
+type FilterHelper<T extends any[], F, R extends any[]> = T extends [infer A, ...infer B] ? (
+    ShouldBeFiltered<A, F> extends true ? FilterHelper<B, F, R> : FilterHelper<B, F, [...R, A]>
+    ) : R;
+export type FilterOut<T extends any[], F> = FilterHelper<T, F, []>
