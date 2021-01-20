12f5826eb17f8a0eb2e2504a5fecec4e2cdc592c
suica <578970498@qq.com>
2021-01-20 21:31:43 +0800

    solve: 612

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,7 @@
-type KebabCase<S> = any;
+type Clear<S extends string> = S extends '-' ? S : (S extends `-${infer A}` ? A : S);
+type KebabCase<S extends string, R extends string = ''> = Clear<S extends `${infer A}${infer B}` ?
+    (IsUpperCase<A> extends true ? KebabCase<B, `${R}-${Lowercase<A>}`> : KebabCase<B, `${R}${A}`>)
+    : `${R}${S}`>;
+
+type IsUpperCase<S extends string> = S extends Uppercase<S> ?
+    (S extends Lowercase<S> ? false : true) : false;
