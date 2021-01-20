fc196f56010e8206d3bfc4236464831fd43867d7
suica <578970498@qq.com>
2021-01-20 21:12:05 +0800

    solve: 610

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,3 @@
-type CamelCase<S> = any;
+type MyCamelCase<S extends string> = S extends `${infer A}-${infer B}${infer C}` ?
+    (B extends Capitalize<B> ? `${A}-${MyCamelCase<`${B}${C}`>}`
+        : `${A}${Capitalize<B>}${MyCamelCase<C>}`) : S
--- questions/610-medium-camelcase/test-cases.ts
+++ questions/610-medium-camelcase/test-cases.ts
@@ -1,15 +1,15 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<CamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
-  Expect<Equal<CamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
-  Expect<Equal<CamelCase<'foo-bar'>, 'fooBar'>>,
-  Expect<Equal<CamelCase<'foo_bar'>, 'foo_bar'>>,
-  Expect<Equal<CamelCase<'foo--bar----baz'>, 'foo-Bar---Baz'>>,
-  Expect<Equal<CamelCase<'a-b-c'>, 'aBC'>>,
-  Expect<Equal<CamelCase<'a-b-c-'>, 'aBC-'>>,
-  Expect<Equal<CamelCase<'ABC'>, 'ABC'>>,
-  Expect<Equal<CamelCase<'-'>, '-'>>,
-  Expect<Equal<CamelCase<''>, ''>>,
-  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+  Expect<Equal<MyCamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
+  Expect<Equal<MyCamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
+  Expect<Equal<MyCamelCase<'foo-bar'>, 'fooBar'>>,
+  Expect<Equal<MyCamelCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<MyCamelCase<'foo--bar----baz'>, 'foo-Bar---Baz'>>,
+  Expect<Equal<MyCamelCase<'a-b-c'>, 'aBC'>>,
+  Expect<Equal<MyCamelCase<'a-b-c-'>, 'aBC-'>>,
+  Expect<Equal<MyCamelCase<'ABC'>, 'ABC'>>,
+  Expect<Equal<MyCamelCase<'-'>, '-'>>,
+  Expect<Equal<MyCamelCase<''>, ''>>,
+  Expect<Equal<MyCamelCase<'😎'>, '😎'>>,
 ]
