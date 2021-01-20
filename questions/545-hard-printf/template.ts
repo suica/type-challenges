5d32f6fb96c8c2bcaf075d0b57b75081dcac97ed
suica <578970498@qq.com>
2021-01-20 21:56:49 +0800

    solve: 545

--- questions/545-hard-printf/template.ts
+++ questions/545-hard-printf/template.ts
@@ -1,1 +1,9 @@
-type Format<T extends string> = any
+type GetTypeBySpec<S extends string> = S extends `${infer A}${infer B}` ? (A extends 'd' ? number : string) : never;
+type FormatHelper<T extends string> = T extends `${infer A}%${infer B}${infer C}` ? [GetTypeBySpec<B>, ...FormatHelper<C>] : []
+type TypeTupleToFunction<T, R> =
+    T extends any[] ?
+        T extends [infer A, ...infer B] ?
+            (a: A) => TypeTupleToFunction<B, R>
+            : R
+        : R;
+type Format<T extends string> = TypeTupleToFunction<FormatHelper<T>, string>;
