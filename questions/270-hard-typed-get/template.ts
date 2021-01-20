dd248094811e0e41a35d470d6b3aa8f39c20a76c
suica <578970498@qq.com>
2021-01-20 14:26:04 +0800

    solve: 270

--- questions/270-hard-typed-get/template.ts
+++ questions/270-hard-typed-get/template.ts
@@ -1,1 +1,9 @@
-type Get<T, K> = string
+type Split<S extends string, P extends string> = S extends `${infer A}${P}${infer C}` ? [A, ...Split<C, P>] : [S]
+type GetHelper<D, K extends any[]> =
+    K['length'] extends 0 ? D : (
+        K[0] extends keyof D ? (
+            (K extends [infer A, ...infer B] ?
+                GetHelper<D[K[0]], B>
+                : never)
+            ) : never)
+type Get<T, K extends string> = GetHelper<T, Split<K, '.'>>
