45c94a72d7c1870767edfcc76593a4b86a765648
suica <578970498@qq.com>
2021-01-20 16:33:12 +0800

    solve: 112

--- questions/112-hard-capitalizewords/template.ts
+++ questions/112-hard-capitalizewords/template.ts
@@ -1,1 +1,10 @@
-type CapitalizeWords<S extends string> = any
+type InclusiveSplit<S extends string, P extends string> = S extends `${infer A}${P}${infer C}` ? [A, P, ...InclusiveSplit<C, P>] : [S]
+type CapitalizeWords<S extends string> = CapitalizeWordsWithSep<CapitalizeWordsWithSep<CapitalizeWordsWithSep<S, '.'>, ' '>, ','>
+type CapitalizeWordsWithSep<S extends string, Sep extends string> = CapitalizeWordsHelper<InclusiveSplit<S, Sep>>
+type CapitalizeWordsHelper<T extends string[]> = T['length'] extends 0 ? '' :
+    T extends [infer First, ...infer R] ?
+        (First extends string ?
+            (R extends string[] ? `${Capitalize<First>}${CapitalizeWordsHelper<R>}` : never)
+            : never)
+        : ''
+type c = CapitalizeWords<'foo bar'>
