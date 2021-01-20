46348d1cf490b27a55bce264ce06fd2aca6b98fb
suica <578970498@qq.com>
2021-01-20 22:05:10 +0800

    solve: 300

--- questions/300-hard-string-to-number/template.ts
+++ questions/300-hard-string-to-number/template.ts
@@ -1,1 +1,2 @@
-type ToNumber<S extends string> = any
+type ToNumberHelper<S extends string, R extends any[] = []> = `${R['length']}` extends S ? R['length'] : ToNumberHelper<S, [1, ...R]>;
+type ToNumber<S extends string> = ToNumberHelper<S>
