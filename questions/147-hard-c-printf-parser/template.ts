4b0eef50ee01526740b04ce2b45f6241fffd0898
suica <578970498@qq.com>
2021-01-20 14:07:22 +0800

    solve: 147

--- questions/147-hard-c-printf-parser/template.ts
+++ questions/147-hard-c-printf-parser/template.ts
@@ -8,4 +8,7 @@ type ControlsMap = {
   p: 'pointer',
 }
 
-type ParsePrintFormat = any
+type Helper<T extends string, R extends string[] = []> = T extends `${infer A}%${infer B}${infer Rest}` ? B extends keyof ControlsMap ? Helper<Rest, [...R, ControlsMap[B]]> : Helper<Rest, R> : R
+type ParsePrintFormat<T extends string> = Helper<T>
+
+type b = ParsePrintFormat<'Hello %s: score is %d.'>
