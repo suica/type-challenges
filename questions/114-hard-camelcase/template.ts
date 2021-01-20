f33dc78130f2c2b274bc8be242a4e8e6743f4505
suica <578970498@qq.com>
2021-01-20 22:49:36 +0800

    solve: 114

--- questions/114-hard-camelcase/template.ts
+++ questions/114-hard-camelcase/template.ts
@@ -1,1 +1,2 @@
-type CamelCase<S extends string> = any
+type CamelCaseHelper<S extends any[]> = S extends [infer A, ...infer B] ? `${Capitalize<ToString<A>>}${CamelCaseHelper<B>}` : '';
+type CamelCase<S extends string> = Uncapitalize<CamelCaseHelper<Split<Lowercase<S>, '_'>>>
