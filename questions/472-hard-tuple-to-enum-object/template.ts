bc4947bdf6a4be9c1ac51e38ec56c521f8f47ac4
suica <578970498@qq.com>
2021-01-20 22:39:13 +0800

    solve: 472

--- questions/472-hard-tuple-to-enum-object/template.ts
+++ questions/472-hard-tuple-to-enum-object/template.ts
@@ -1,1 +1,4 @@
-type Enum<T extends readonly string[], N extends boolean = false> = any
+type ToString<A extends any> = A extends string ? `${A}` : never;
+type EnumHelperCount<T extends readonly any[], Switch extends boolean, Count extends any[] = []> =
+    [...T] extends [infer A, ...infer B] ? AppendToObject<EnumHelperCount<B, Switch, [...Count, 1]>, Capitalize<ToString<A>>, Switch extends true ? Count['length'] : A> : {}
+type Enum<T extends readonly string[], N extends boolean = false> = Readonly<EnumHelperCount<T, N>>
