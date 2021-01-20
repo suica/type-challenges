cfe221aa90a05d24bd81a4a3288b86a4b0778631
suica <578970498@qq.com>
2021-01-21 00:01:08 +0800

    solve: 553

--- questions/553-hard-deep-object-to-unique/template.ts
+++ questions/553-hard-deep-object-to-unique/template.ts
@@ -1,1 +1,8 @@
-type DeepObjectToUniq<O extends object> = any
+// or declare const sym : unique symbol;
+const sym = Symbol();
+type DeepObjectToUniq<O extends any, prefix extends string = '', Root = O> = O extends number ? O :
+    (O extends object ?
+        { [x in keyof O]: DeepObjectToUniq<O[x], `${ToString<x>}${prefix}`, Root> }
+        : never) & {
+    readonly [sym]?: [Root, prefix];
+};
