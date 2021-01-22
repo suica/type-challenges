9a2b6b89bfcdbcd218ca29ffd8481a3702216230
suica <578970498@qq.com>
2021-01-23 01:38:25 +0800

    solve: 462

--- questions/462-extreme-currying-2/template.ts
+++ questions/462-extreme-currying-2/template.ts
@@ -1,1 +1,2 @@
-declare function DynamicParamsCurrying(fn: any): any
+declare function DynamicParamsCurrying<T extends any, P extends any[]>(fn: (...args: [...P]) => T): CurringHelper<[], P, T>
+type CurringHelper<Old extends any[], Total extends any[], Ret> = Old['length'] extends Total['length'] ? Ret : <New extends any[]>(...args: New) => CurringHelper<[...Old, ...New], Total, Ret>
